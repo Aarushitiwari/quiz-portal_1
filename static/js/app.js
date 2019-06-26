118a923087b811dc8323908444822be82db3ca2d
dragano999 <42536880+dragano316@users.noreply.github.com>
2019-06-26 19:35:23 +0530

    updated files

---
+++ static/js/app.js
@@ -0,0 +1,19 @@
+function signup() {
+	// body...
+	// var sign=document.querySelector('.signinn');
+	// sign.style.background= 'linear-gradient(to top,rgba(0,0,80,1),rgba(255,127,80,.5))';
+	location.href='H:\\Project\\quiz_portal_1\\templates\\quiz_signup.html';
+}
+function login() {
+	// body...
+	location.href='H:\\Project\\quiz_portal_1\\templates\\login.html';
+}
+
+// function changeactive() {
+	// body...
+	// document.querySelector('.info-1').classList.toggle('active');
+	// document.querySelector('.info-2').classList.toggle('active');
+	// about.classList.toggle('active');
+	// event.classList.toggle('active');
+	// location.href='H:\\Project\\quiz_portal_1\\templates\\login.html';
+// }
